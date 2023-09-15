8e3e274bdfd69f2e0631bd801a9b9df3d2a18082
AntonelloStefania <antonelloste94@gmail.com>
2023-09-15 13:13:12 +0200

    visualizzata mappa

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,20 +1,29 @@
 <script>
-// const map = tt.map({
-//     key: '<NvRVuGxMpACPuu2WUR93HOEvbVfg2g9A>',
-//     container: 'map'
-// });
+ import tt from '@tomtom-international/web-sdk-maps';
 export default {
+    data() {
+        return {
+            map: null, 
+        }
+    },mounted() {
+        this.map = tt.map({
+     key: '3gixrb8T1tvg0AxLRGVxYkwtVlKmQGvj',
+     container: 'map'
+    });
 
-
+    this.map.setCenter([45.55002 , 8.93635]); // Sostituisci con le coordinate desiderate
+        //this.map.setZoom(zoomLevel);
+    },
+    
 }
 </script>
 
 <template lang="">
-    <div style="width: 100%; height: 100%; margin: 0; padding: 0;">
-        <div id="map" style="width: 100%; height: 100%;"></div>
+    <div style="width: 100%; height: 400px; margin: 0; padding: 0;">
+        <div id="map" style="width: 100%; height: 400px;"></div>
     </div>
 </template>
 
-<style lang="">
+<style lang="scss">
     
 </style>
