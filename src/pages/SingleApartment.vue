0247852dd4e0a77c5ecd9906f974e8c9ba12a26c
alep-2000 <alessiopiras056@gmail.com>
2023-09-21 11:00:41 +0200

    Modify

--- src/pages/SingleApartment.vue
+++ src/pages/SingleApartment.vue
@@ -19,8 +19,8 @@ export default {
             message: '',
             store,
             errors: {},
-            apartment_id:'',
-            messageForm:'',
+            apartment_id: '',
+            messageForm: '',
             loading: false,
         }
     },
@@ -40,123 +40,125 @@ export default {
             })
         },
 
-        sendForm(){
-            this.loading=true;
-         const data = {
-             name:this.name,
-             surname:this.surname,
-             email:this.email,
-             message:this.message,
-             apartment_id:this.apartment_id,
-         }
-         console.log(data)
-         const urlMessage= `http://127.0.0.1:8000/api/messages`
-         axios.post(urlMessage,data)
-         .then(resp=>{
-             console.log(resp)
-             this.success = resp.data.success
-             if(this.success){
-                 this.name =""
-                 this.surname = ""
-                 this.email = ""
-                 this.message =""
-                 this.errors = ""
-                 this.messageForm = "Form inviato sul cesso"
-                 this.$router.push({ name: 'thank-you' })
-             }else{
-                 this.errors = resp.data.errors;
-             }
+        sendForm() {
+            this.loading = true;
+            const data = {
+                name: this.name,
+                surname: this.surname,
+                email: this.email,
+                message: this.message,
+                apartment_id: this.apartment_id,
+            }
 
-             this.loading = false
-            
-         })
+            this.errors = {}
+            console.log(data)
+            const urlMessage = `http://127.0.0.1:8000/api/messages`
+            axios.post(urlMessage, data)
+                .then(resp => {
+                    console.log(resp)
+                    this.success = resp.data.success
+                    if (this.success) {
+                        this.name = ""
+                        this.surname = ""
+                        this.email = ""
+                        this.message = ""
+                        this.errors = ""
+                        this.messageForm = "Form inviato sul cesso"
+                        this.$router.push({ name: 'thank-you' })
+                    } else {
+                        this.errors = resp.data.errors;
+                    }
 
+                    this.loading = false
 
-//SECONDA PROVA FUNZIONAAAAAAAAAAA
-// const urlMessage = `http://127.0.0.1:8000/api/messages`;
-
-// // Crea un oggetto FormData per i dati del modulo
-//  const formData = new FormData();
-//  formData.append('name', document.getElementById('name').value);
-//  formData.append('surname', document.getElementById('surname').value);
-//  formData.append('email', document.getElementById('email').value);
-//  formData.append('message', document.getElementById('message').value);
-//  formData.append('apartment_id', this.apartment_id)
-
-//  // Crea una richiesta XMLHttpRequest
-//  const xhr = new XMLHttpRequest();
-//  xhr.open('POST', urlMessage, true);
-
-//  // Imposta il gestore dell'evento per la risposta
-//  xhr.onload = function () {
-//      if (xhr.status === 200) {
-//          // Risposta dal server
-//          const response = JSON.parse(xhr.responseText);
-//          console.log(response);
-
-//          this.success = response.success;
-
-//          if (this.success === true) {
-// //             // Form inviato con successo, reimposta i campi e gestisci la conferma
-           
-//             document.getElementById('name').value = '';
-//             document.getElementById('surname').value = '';
-//             document.getElementById('email').value = '';
-//             document.getElementById('message').value = '';
-
-          
-//          } else {
-//              // Gestisci gli errori
-//              this.errors = response.errors;
-//          }
-//      } else {
-//          console.error('Errore nella richiesta:', xhr.status);
-//      }
-//  };
-
-// // // Invia la richiesta con i dati del modulo
-//  xhr.send(formData);
-    
-
-//TERZA PROVA
-
-// Crea un oggetto FormData con i dati del form
-// let data = new FormData();
-// data.append("name", this.name);
-// data.append("surname",this.surname);
-// data.append("apartment_id", this.apartment_id)
-// data.append("email", this.email);
-// data.append("message", this.message);
-
-// // Crea le opzioni della richiesta
-// let options = {
-//   method: "POST",
-//   body: data,
-// };
-// console.log(options)
-// // Invia la richiesta con la Fetch API
-// fetch("http://127.0.0.1:8000/api/messages", options)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     // Gestisci la risposta del server
-//     this.success = data.success;
-//     if (!this.success) {
-//       this.errors = data.errors;
-//     } else {
-//       this.name = "";
-//       this.email = "";
-//       this.message = "";
-//       this.surname="";
-      
-//     }
-
-//     this.loading = false;
-//   });
+                })
 
 
-}
-       
-     }
+            //SECONDA PROVA FUNZIONAAAAAAAAAAA
+            // const urlMessage = `http://127.0.0.1:8000/api/messages`;
+
+            // // Crea un oggetto FormData per i dati del modulo
+            //  const formData = new FormData();
+            //  formData.append('name', document.getElementById('name').value);
+            //  formData.append('surname', document.getElementById('surname').value);
+            //  formData.append('email', document.getElementById('email').value);
+            //  formData.append('message', document.getElementById('message').value);
+            //  formData.append('apartment_id', this.apartment_id)
+
+            //  // Crea una richiesta XMLHttpRequest
+            //  const xhr = new XMLHttpRequest();
+            //  xhr.open('POST', urlMessage, true);
+
+            //  // Imposta il gestore dell'evento per la risposta
+            //  xhr.onload = function () {
+            //      if (xhr.status === 200) {
+            //          // Risposta dal server
+            //          const response = JSON.parse(xhr.responseText);
+            //          console.log(response);
+
+            //          this.success = response.success;
+
+            //          if (this.success === true) {
+            // //             // Form inviato con successo, reimposta i campi e gestisci la conferma
+
+            //             document.getElementById('name').value = '';
+            //             document.getElementById('surname').value = '';
+            //             document.getElementById('email').value = '';
+            //             document.getElementById('message').value = '';
+
+
+            //          } else {
+            //              // Gestisci gli errori
+            //              this.errors = response.errors;
+            //          }
+            //      } else {
+            //          console.error('Errore nella richiesta:', xhr.status);
+            //      }
+            //  };
+
+            // // // Invia la richiesta con i dati del modulo
+            //  xhr.send(formData);
+
+
+            //TERZA PROVA
+
+            // Crea un oggetto FormData con i dati del form
+            // let data = new FormData();
+            // data.append("name", this.name);
+            // data.append("surname",this.surname);
+            // data.append("apartment_id", this.apartment_id)
+            // data.append("email", this.email);
+            // data.append("message", this.message);
+
+            // // Crea le opzioni della richiesta
+            // let options = {
+            //   method: "POST",
+            //   body: data,
+            // };
+            // console.log(options)
+            // // Invia la richiesta con la Fetch API
+            // fetch("http://127.0.0.1:8000/api/messages", options)
+            //   .then((response) => response.json())
+            //   .then((data) => {
+            //     // Gestisci la risposta del server
+            //     this.success = data.success;
+            //     if (!this.success) {
+            //       this.errors = data.errors;
+            //     } else {
+            //       this.name = "";
+            //       this.email = "";
+            //       this.message = "";
+            //       this.surname="";
+
+            //     }
+
+            //     this.loading = false;
+            //   });
+
+
+        }
+
+    }
 }
 </script>
 
@@ -195,40 +197,36 @@ export default {
                                 <div class="name">
                                     <label for="" class="form-label">Nome*</label>
                                     <input type="text" name="name" id="name" class="form-control" placeholder=""
-                                        aria-describedby="helpId" required v-model="name">
-<!-- 
-                                    <div class="alert alert-danger" role="alert" v-for="error in errors.name">
+                                        aria-describedby="helpId" required v-model="name" :class="errors.name ? 'is-invalid' : '' ">
+                                    <div class="alert alert-danger" role="alert" v-for="(error,index) in errors.name" :key="index">
                                         {{ error }}
-                                    </div> -->
+                                    </div>
                                 </div>
                                 <div class="surname">
                                     <label for="" class="form-label">Cognome*</label>
                                     <input type="text" name="surname" id="surname" class="form-control" placeholder=""
-                                        aria-describedby="helpId" required v-model="surname">
-<!-- 
-                                    <div class="alert alert-danger" role="alert" v-for="error in errors.surname">
+                                        aria-describedby="helpId" required v-model="surname" :class="errors.surname ? 'is-invalid' : '' ">
+                                    <div class="alert alert-danger" role="alert" v-for="(error,index) in errors.surname" :key="index">
                                         {{ error }}
-                                    </div> -->
+                                    </div>
                                 </div>
                             </div>
                             <div class="email">
                                 <label for="" class="form-label">Email*</label>
                                 <input type="email" name="email" id="email" class="form-control" placeholder=""
-                                    aria-describedby="helpId" required v-model="email">
-<!-- 
-                                <div class="alert alert-danger" role="alert" v-for="error in errors.email">
+                                    aria-describedby="helpId" required v-model="email" :class="errors.email ? 'is-invalid' : '' ">
+                                <div class="alert alert-danger" role="alert" v-for="(error,index) in errors.email" :key="index">
                                     {{ error }}
-                                </div> -->
+                                </div>
                             </div>
                             <div class="message">
                                 <label for="" class="form-label">Messaggio*</label>
                                 <textarea rows="3" cols="50" name="message" id="message" class="form-control" placeholder=""
-                                    aria-describedby="helpId" required v-model="message">
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        </textarea>
-<!-- 
-                                <div class="alert alert-danger" role="alert" v-for="error in errors.message">
+                                    aria-describedby="helpId" required v-model="message" :class="errors.message ? 'is-invalid' : '' ">
+                                </textarea>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+                                <div class="alert alert-danger" role="alert" v-for="(error,index) in errors.message" :key="index">
                                     {{ error }}
-                                </div> -->
+                                </div>
                             </div>
                             <div class="col-12 text-center my-5">
 
@@ -253,6 +251,8 @@ export default {
 @use '../styles/generals.scss' as *;
 
 @keyframes spinner-border {
-  to { transform: rotate(360deg) #{"/* rtl:ignore */"}; }
+    to {
+        transform: rotate(360deg) #{"/* rtl:ignore */"};
+    }
 }
 </style>
