ccd91d852eded8f2a8633ac7aadf320ace2686e9
AntonelloStefania <antonelloste94@gmail.com>
2023-09-29 03:25:06 +0200

    corretta visualizzazione show

--- src/pages/SingleApartment.vue
+++ src/pages/SingleApartment.vue
@@ -194,153 +194,155 @@ export default {
 </script>
 
 <template lang="">
-    <div class="container-fluid navbar-container">
-        <div class="row justify-content-between w-100">
-            <div class="col-2 py-3 d-flex justify-content-end">
-                <router-link :to="{name: 'home'}"  class="fw-bold" style="text-decoration:none; color:#3a537e;"><i class="fa-regular fa-circle-left" style="color: #3a537e;"></i> Annunci in Evidenza </router-link>
+   <div class="container-fluid navbar-container">
+    <div class="row justify-content-between w-100">
+        <div class="d-flex col-6 justify-content-center">
+            <div class="col-6 py-3 d-flex justify-content-center">
+                <router-link :to="{ name: 'home' }" class=" d-flex align-items-center " style="text-decoration: none; color: #3a537e;">
+                    <div class="col-auto">
+                        <i class="fa-regular fa-circle-left me-2" style="color: #3a537e;"></i>
+                    </div>
+                    <div class="col">
+                        <span>In Evidenza</span>
+                    </div>
+                </router-link>
             </div>
-            <div class="col-2 py-3 ">
+        </div>
+        <div class="d-flex col-6 justify-content-center">
+            <a href="#bottom" style="text-decoration: none; color: #3a537e;" class=" d-flex align-items-center ">
+                <div class="col-auto">
+                    <span>Contatta l'Host</span>
+                </div>
+                <div class="col">
+                    <i class="fa-regular fa-circle-down ms-2" style="color: #3a537e;"></i>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
 
-                <a href="#bottom" style="text-decoration:none; color:#3a537e;" class="fw-bold"> Contatta l'Host <i class="fa-regular fa-circle-down"  style=" color:#3a537e;"></i></a>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-8 offset-2 text-center mb-5">
+            <h2 class="my-3"><span class="brand">Dettagli</span></h2>
+            <p class="text-center">
+                Scopri tutti i dettagli di questa straordinaria opportunità. Prenditi il tempo per esaminare attentamente ogni aspetto e assicurarti che questo sia il luogo perfetto per il tuo prossimo soggiorno. Se vuoi rendere la tua esperienza ancora più completa, dai un'occhiata alle foto aggiuntive caricate dagli host. Ricorda che ogni annuncio è unico, quindi non esitare a chiedere all'host qualsiasi domanda tu possa avere. Siamo qui per aiutarti a trovare l'alloggio ideale su <span class="brand">BoolBnB</span>. Buona esplorazione!
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid bg-beige py-3">
+    <div class="container">
+        <div class="col-12 text-center mt-2 mb-4">
+            <h2>{{ apartment.title }}</h2>
+        </div>
+        <div class="col-12 d-flex flex-column flex-lg-row">
+            <div class="col-12 col-lg-4 d-flex align-items-center order-2 order-lg-1 mx-lg-3 mb-3">
+                <img :src="`${store.baseUrl}/storage/${apartment.cover}`" class="cover">
+            </div>
+            <div class="col-12 m-0 col-lg-8 text-center order-2 order-lg-2 justify-content-center mb-3">
+                <div class="container gal-container">
+                    <div class="gal-item d-flex flex-wrap justify-content-center">
+                        <div class="box col-12 col-lg-5 m-2 img-container" v-for="photo in apartment.photos" :key="photo.id">
+                            <img :src="`${store.baseUrl}/storage/${photo.url}`" class="col-6" style="border-radius: 1.25rem">
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-     
-        <div class="container-fluid">
-            <div class="row ">
-                <div class="col-8 offset-2 text-center mb-5">
-                    <h2 class="my-3"><span class="brand">Dettagli</span></h2>
-                    <p class="text-center">Scopri tutti i dettagli di questa straordinaria opportunità. Prenditi il tempo per esaminare attentamente ogni aspetto e assicurarti che questo sia il luogo perfetto per il tuo prossimo soggiorno. Se vuoi rendere la tua esperienza ancora più completa, dai un'occhiata alle foto aggiuntive caricate dagli host. Ricorda che ogni annuncio è unico, quindi non esitare a chiedere all'host qualsiasi domanda tu possa avere. Siamo qui per aiutarti a trovare l'alloggio ideale su <span class="brand">BoolBnB</span>. Buona esplorazione!</p>
-                </div>
-                <div class="container-fluid bg-beige py-3 ">
-                    <div class="container">
-                        <div class="col-12 text-center mt-2 mb-4">
-                            <h2>
-                                {{apartment.title}}
-                            </h2>
-                        </div>
-                        <div class="col-12 d-flex flex-column flex-lg-row ">
-                            <div class="col-12  col-lg-4 d-flex align-items-center order-2 order-lg-1 mx-lg-3 mb-3">
-                                <img :src="`${store.baseUrl}/storage/${apartment.cover}`" class="cover">
-                            </div>
-                            <div class="col-12 m-0 col-lg-8  text-center order-2 order-lg-2  justify-content-center mb-3 " >
-                                <div class="container gal-container ">
-                                    <div class="gal-item d-flex flex-wrap justify-content-center">
-                                        <div class="box col-12 col-lg-5 m-2  img-container  " v-for="photo in apartment.photos" :key="photo.id">
-                                            <img :src="`${store.baseUrl}/storage/${photo.url}`" class="col-6" style="border-radius:1.25rem" >
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- {{-- FINE SEZIONE IMMAGINI --}} -->
-        <div class="container my-5">
-            <div class="row">
-                <div class="col-12 col-md-6 flex-column d-flex align-items-center d-md-block  col-lg-3 my-4 my-lg-0 text-lg-start">
-                    <h5 class="mb-3 d-flex "><span class="brand">Alloggio</span></h5>
-                        <ul class="list-unstyled ">
-                            <li class=" ">
-                                 <img :src="apartment.type.icons"  class="me-2 type-icon" alt="">  
-                                <span class="fw-bold brand">{{apartment.type.name}}</span>   
-                            </li>
-                            <li class="" >
-                                <span class="" ><span class="brand">Grandezza</span> alloggio: </span><span class="fw-bold " >{{apartment.mq}} <span class="fs-5">&#x33A1;</span> </span>
-                            </li>
-                            <li class="">
-                                <span class="">Numero di <span class="brand">Stanze:</span> </span> <span class="mx-2 fw-bold ">{{apartment.n_rooms}}<i class="fa-solid fa-building ms-2 align-self-center" style="color: #4f5153;"></i></span>
-                            </li>
-                            <li class="">
-                                <span class="">Numero di <span class="brand">Bagni:</span> </span> <span class="ms-2 fw-bold ">{{apartment.n_wc}}<i class="fa-solid fa-toilet-paper ms-2" style="color: #4f5153;"></i></span>
-                            </li>
-                            <li class="">
-                                <span class="">Numero di <span class="brand">Letti:</span> </span> <span class="ms-2 fw-bold ">{{apartment.n_beds}}<i class="fa-solid fa-bed ms-2 align-self-center" style="color: #4f5153;"></i></span>
-                            </li>
-                        </ul>
-                    
-                </div>
-                <div class="col-12 col-md-6 flex-column d-flex align-items-center d-md-block  col-lg-3 my-4 my-lg-0 text-lg-start">
-                    <h5 class="mb-3">I <span class="brand">Servizi</span></h5>
-                    <ul class="list-unstyled apartment-list " v-for="service in apartment.services" :key="service.id" >
-                        <li class="">
-                           <img :src="service.icons" alt="" class="type-icon me-2"> 
-                            <span class="">{{service.name}}</span>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-12  col-lg-6 text-center my-4 my-lg-0 text-md-start">
-                    <h5 class="mb-3">Il <span class="brand">Pernottamento</span></h5>
-                   
-                        <ul class="list-unstyled apartment-list ">
-                            <li class="">
-                               <span><span class="brand">Prezzo</span> per Notte: </span><span class="ms-2 fw-bold">{{apartment.price}}&euro;</span>
-                            </li>
-                            <li class="">
-                               <span><span class="brand">Indirizzo</span>: </span><span class="ms-2 ">{{apartment.address}} <i class="fa-solid fa-location-dot ms-1" style="color: #4f5153;"></i></span>
-                            </li>
-                            <li class="">
-                                <span><span class="brand">Descrizione</span>: </span><span class="ms-2 w-break">{{apartment.description}}  </span>
-                            
-                             </li>
-                        </ul>
-                   
-                </div>
-            </div>
-        </div>
-    <div>
+</div>
 
-    <div class="container my-5">
-        <div class="col-12 text-center mb-5">
-            <h2 class="">Contatta l'Host su <span class="brand">BoolBnB</span></h2>
-            <p>Hai domande sull'annuncio o hai bisogno di informazioni aggiuntive dall'host? Utilizza il modulo di contatto qui sotto per metterti in contatto direttamente con l'host. Fai la tua prenotazione in modo sicuro e senza stress su <span class="brand">BoolBnB!</span></p>
+<div class="container my-5">
+    <div class="row">
+        <div class="col-12 col-md-6 flex-column d-flex align-items-center d-md-block col-lg-3 my-4 my-lg-0 text-lg-start">
+            <h5 class="mb-3 d-flex"><span class="brand">Alloggio</span></h5>
+            <ul class="list-unstyled">
+                <li>
+                    <img :src="apartment.type.icons" class="me-2 type-icon" alt="">
+                    <span class="fw-bold brand">{{ apartment.type.name }}</span>
+                </li>
+                <li>
+                    <span><span class="brand">Grandezza</span> alloggio:</span>
+                    <span class="fw-bold">{{ apartment.mq }} <span class="fs-5">&#x33A1;</span></span>
+                </li>
+                <li>
+                    <span>Numero di <span class="brand">Stanze:</span></span>
+                    <span class="mx-2 fw-bold">{{ apartment.n_rooms }}<i class="fa-solid fa-building ms-2 align-self-center" style="color: #4f5153;"></i></span>
+                </li>
+                <li>
+                    <span>Numero di <span class="brand">Bagni:</span></span>
+                    <span class="ms-2 fw-bold">{{ apartment.n_wc }}<i class="fa-solid fa-toilet-paper ms-2" style="color: #4f5153;"></i></span>
+                </li>
+                <li>
+                    <span>Numero di <span class="brand">Letti:</span></span>
+                    <span class="ms-2 fw-bold">{{ apartment.n_beds }}<i class="fa-solid fa-bed ms-2 align-self-center" style="color: #4f5153;"></i></span>
+                </li>
+            </ul>
         </div>
-        <div class="row">
-            <div class="col-12 col-md-8 offset-md-2 pe-lg-0 card d-flex flex-row justify-content-between">
-                <div class="col-10 py-4  offset-1 offset-xl-0 col-xl-6 ">
-                    <form @submit.prevent="sendForm()">
-                    <div class="">
+        <div class="col-12 col-md-6 flex-column d-flex align-items-center d-md-block col-lg-3 my-4 my-lg-0 text-lg-start">
+            <h5 class="mb-3">I <span class="brand">Servizi</span></h5>
+            <ul class="list-unstyled apartment-list" v-for="service in apartment.services" :key="service.id">
+                <li>
+                    <img :src="service.icons" alt="" class="type-icon me-2">
+                    <span>{{ service.name }}</span>
+                </li>
+            </ul>
+        </div>
+        <div class="col-12 col-lg-6 text-center my-4 my-lg-0 text-md-start">
+            <h5 class="mb-3">Il <span class="brand">Pernottamento</span></h5>
+            <ul class="list-unstyled apartment-list">
+                <li>
+                    <span><span class="brand">Prezzo</span> per Notte:</span>
+                    <span class="ms-2 fw-bold">{{ apartment.price }}&euro;</span>
+                </li>
+                <li>
+                    <span><span class="brand">Indirizzo</span>:</span>
+                    <span class="ms-2">{{ apartment.address }} <i class="fa-solid fa-location-dot ms-1" style="color: #4f5153;"></i></span>
+                </li>
+                <li>
+                    <span><span class="brand">Descrizione</span>:</span>
+                    <span class="ms-2 w-break">{{ apartment.description }}</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="container my-5">
+    <div class="col-12 text-center mb-5">
+        <h2 class="">Contatta l'Host su <span class="brand">BoolBnB</span></h2>
+        <p>Hai domande sull'annuncio o hai bisogno di informazioni aggiuntive dall'host? Utilizza il modulo di contatto qui sotto per metterti in contatto direttamente con l'host. Fai la tua prenotazione in modo sicuro e senza stress su <span class="brand">BoolBnB!</span></p>
+    </div>
+    <div class="row">
+        <div class="col-12 col-md-8 offset-md-2 pe-lg-0 card d-flex flex-row justify-content-between">
+            <div class="col-10 py-4 offset-1 offset-xl-0 col-xl-6">
+                <form @submit.prevent="sendForm()">
+                    <div>
                         <div class="name">
-                            <label for="" class="form-label">Nome*</label>
-                            <input type="text" name="name" id="name" class="form-control custom-form px-2" placeholder=""
-                                aria-describedby="helpId" required v-model="name">
-    
-                            <div class="alert alert-danger" role="alert" v-for="error in errors.name">
-                                {{ error }}
-                            </div> 
+                            <label for="name" class="form-label">Nome*</label>
+                            <input type="text" name="name" id="name" class="form-control custom-form px-2" placeholder="" aria-describedby="helpId" required v-model="name">
+                            <div class="alert alert-danger" role="alert" v-for="error in errors.name">{{ error }}</div>
                         </div>
                         <div class="surname" id="bottom">
-                            <label for="" class="form-label">Cognome*</label>
-                            <input type="text" name="surname" id="surname" class="form-control custom-form px-2" placeholder=""
-                                aria-describedby="helpId" required v-model="surname">
-    
-                            <div class="alert alert-danger" role="alert" v-for="error in errors.surname">
-                                {{ error }}
-                            </div>
+                            <label for="surname" class="form-label">Cognome*</label>
+                            <input type="text" name="surname" id="surname" class="form-control custom-form px-2" placeholder="" aria-describedby="helpId" required v-model="surname">
+                            <div class="alert alert-danger" role="alert" v-for="error in errors.surname">{{ error }}</div>
                         </div>
                     </div>
                     <div class="email">
-                        <label for="" class="form-label">Email*</label>
-                        <input type="email" name="email" id="email" class="form-control custom-form px-2" placeholder=""
-                            aria-describedby="helpId" required v-model="email">
-    
-                         <div class="alert alert-danger" role="alert" v-for="error in errors.email">
-                            {{ error }}
-                        </div> 
+                        <label for="email" class="form-label">Email*</label>
+                        <input type="email" name="email" id="email" class="form-control custom-form px-2" placeholder="" aria-describedby="helpId" required v-model="email">
+                        <div class="alert alert-danger" role="alert" v-for="error in errors.email">{{ error }}</div>
                     </div>
                     <div class="message">
-                        <label for="" class="form-label">Messaggio*</label>
-                        <textarea rows="3" cols="50" name="message" id="message" class="form-control custom-form p-2" placeholder=""
-                            aria-describedby="helpId" required v-model="message"></textarea>
-                        <div class="alert alert-danger" role="alert" v-for="error in errors.message">
-                            {{ error }}
-                        </div>
+                        <label for="message" class="form-label">Messaggio*</label>
+                        <textarea rows="3" cols="50" name="message" id="message" class="form-control custom-form p-2" placeholder="" aria-describedby="helpId" required v-model="message"></textarea>
+                        <div class="alert alert-danger" role="alert" v-for="error in errors.message">{{ error }}</div>
                     </div>
                     <div class="text-center my-3">
-    
-                        <div  v-if="loading" class="disabled">
+                        <div v-if="loading" class="disabled">
                             <button class="btn blue-btn" type="button" disabled>
                                 <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                                 Loading...
@@ -348,16 +350,14 @@ export default {
                         </div>
                         <button v-else class="btn blue-btn" type="submit">Invia</button>
                     </div>
-                    </form>
-                </div>
-                <div class="col-5 card-image d-none   d-lg-block">
-                    <img src="../assets/apartments/contact-2.jpg"  class=" img-fluid h-100 contact-img" alt="">
-                </div>
+                </form>
+            </div>
+            <div class="col-5 card-image d-none d-lg-block">
+                <img src="../assets/apartments/contact-2.jpg" class="img-fluid h-100 contact-img" alt="">
             </div>
         </div>
     </div>
 </div>
-
 </template>
 
 <style lang="scss">
