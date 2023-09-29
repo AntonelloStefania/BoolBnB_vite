dd3b735582c3a90a5e007fd8f41f711ea7983416
AntonelloStefania <antonelloste94@gmail.com>
2023-09-29 18:37:56 +0200

    add stile page not found e thankyou

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -5,14 +5,39 @@ export default {
 </script>
 
 <template lang="">
-    <div>
+    <div class="container-fluid bg-beige">
         <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <h1 class="text-center my-5 py-5 text-bg-danger">
-                        Error 404: Page Not Found!
+                <div class="col-12 my-5">
+                    <h1 class="text-center   ">
+                        Error 404: Pagina Non Trovata!
                     </h1>
+                    <div class="col-12 text-center">
+                        <p>
+                            Ci dispiace, ma la pagina che stai cercando non è stata trovata. Questo potrebbe essere dovuto a un URL errato, a una pagina rimossa o a un errore di digitazione. Ti invitiamo a controllare l'URL e a tornare alla pagina principale per continuare a navigare nel nostro sito.
+                        </p>
+                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-12 text-center">
+                <p>
+                    Non disperarti, si tratta solo di un piccolo contrattempo nella tua esplorazione. Potresti voler dare un'occhiata a qualche altra sezione del nostro sito o tornare alla pagina principale per scoprire nuove opportunità. Per qualsiasi domanda o assistenza, siamo sempre a disposizione. Grazie per la tua pazienza.
+                </p>
+            </div>
+            
+            <div class="col-12 text-center">
+                <p>
+                    Nel frattempo, puoi esplorare altre sezioni del nostro sito o utilizzare la barra di ricerca per trovare ciò che stai cercando. Se il problema persiste o se hai bisogno di assistenza, non esitare a contattare il nostro servizio clienti. Siamo qui per aiutarti.
+                </p>
+            </div>
+            <div class="col-12 text-center">
+                <p>
+                    <span class="brand">Ci scusiamo per l'inconveniente e ti ringraziamo per la tua comprensione.</span>
+                </p>
             </div>
         </div>
     </div>
--- src/pages/ThankYou.vue
+++ src/pages/ThankYou.vue
@@ -5,20 +5,39 @@ export default {
 </script>
 
 <template lang="">
-    <div>
-        <section class="bg-light">
+    <div class="container-fluid bg-beige">
+       
         <div class="container">
-            <div class="row text-center mb-3 p-2">
-                <div class="col-12">
-                    <h1>Grazie per averci contattato</h1>
+            <div class="row  mb-3 p-2">
+                <div class="col-12 my-5">
+                    <h1 class="text-center">Grazie per averci contattato tramite <span class="brand">BoolBnB</span></h1>
                 </div>
-                <div class="col-12">
-                    <p>Un nostro operatore le risponderà non appena possibile</p>
-                    <p>Nel frattempo le auguro una buona navigazione sul nostro sito</p>
+                <div class="col-12 text-center">
+                    <p>
+                        Siamo felici di averti tra i nostri utenti su BoolBnB e di averti visto interessato a uno dei nostri appartamenti. Abbiamo ricevuto il tuo messaggio e ti ringraziamo per aver contattato l'host tramite la nostra piattaforma.
+                    </p>
+                    <p>
+                        L'host è stato informato della tua richiesta e delle tue domande e si prenderà cura di rispondere nel più breve tempo possibile. Nel frattempo, se hai ulteriori domande o necessiti di assistenza, non esitare a contattarci direttamente tramite il nostro servizio clienti.
+                    </p>
                 </div>
             </div>
         </div>
-    </section>
+   
+    </div>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-12 text-center">
+                <p>
+                     Speriamo che tu possa trovare l'alloggio perfetto per le tue esigenze e che il tuo soggiorno su <span class="brand">BoolBnB</span> sia piacevole e soddisfacente.
+                 </p>
+            </div>
+            <div class="col-12 text-end my-5">
+                Cordiali saluti, <br>
+
+                Il Team di <span class="brand">BoolBnB</span>
+            </div>
+        </div>
+        
     </div>
 </template>
 
